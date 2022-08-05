fa8fc4805eee27e422fd4a4665aa67952e4f3f7c
Andy McBeath <mcbeath1@gmail.com>
2022-08-05 13:15:14 -0500

    Added UserScores Join Table

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,3 @@
 class Score < ApplicationRecord
-  belongs_to :user
+  has_many :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_05_165657) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_05_181041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_05_165657) do
     t.string "title"
     t.string "composer"
     t.string "image_file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_scores", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "score_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
