fe219b53d8059625098d11a4b8d67e850fac6a0e
Andy McBeath <mcbeath1@gmail.com>
2022-09-15 18:22:08 -0500

    Cleaned up some things

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,10 @@
-# User.create!([{ name: "Chad", email: "chad@example.com", password: "password" },
-#               { name: "Karen", email: "karen@example.com", password: "password" }])
-# Score.create!([title: "Brandenburg Concerto", composer: "Johann Sebastian Bach", score: "https://cdn.handelandhaydn.org/app/uploads/2019/08/26143120/6483952991_b5a94334b1_o-1.jpg"])
+User.create!([{ name: "Chad", email: "chad@example.com", password: "password" },
+              { name: "Karen", email: "karen@example.com", password: "password" }])
 
-# Score.create!([title: "Symphony Number 1", composer: "Ludwig van Beethoven", score: "https://www.stretta-music.com/media/images/187/471187_detail-01.jpg"])
+Score.create!([title: "Mambo", composer: "Anonymous", score: "62ffbe9034167b00133bd2e1"])
 
-# Score.create!([title: "Mambo", composer: "Anonymous", score: "62ffbe9034167b00133bd2e1"])
+Score.create!([title: "Freight Train", composer: "Anonymous", score: "630698c8141a680014102256"])
 
-# Score.create!([title: "Freight Train", composer: "Anonymous", score: "630698c8141a680014102256"])
-
-# Score.create!([title: "Excerpt from Clarinet Quintet, K. 581", composer: "Wolfgang Amadeus Mozart", score: "630cf06800726b001244573f"])
+Score.create!([title: "Excerpt from Clarinet Quintet, K. 581", composer: "Wolfgang Amadeus Mozart", score: "630cf06800726b001244573f"])
 
 Score.create!([title: "Mandoline", composer: "Claude Debussy", score: "630e6f6c8f7f570012fef4fe"])
